9e5c5a13b8e0ca9a13edb911b410abcc8ed01fc9
ruslan7up <ruslan7up@gmail.com>
2017-02-26 17:07:17 +0600

    updates

--- app/src/main/java/kz/kineu/mycollege/Fragments/ScheduleFragment.java
+++ app/src/main/java/kz/kineu/mycollege/Fragments/ScheduleFragment.java
@@ -12,6 +12,8 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Calendar;
+
 import kz.kineu.mycollege.Adapters.ScheduleAdapter;
 import kz.kineu.mycollege.Entities.Schedule;
 import kz.kineu.mycollege.R;
@@ -42,6 +44,15 @@ public class ScheduleFragment extends Fragment {
         ViewPager viewPager = (ViewPager) view.findViewById(R.id.vpSchedule);
         ScheduleAdapter adapter = new ScheduleAdapter(getActivity().getSupportFragmentManager(),(Schedule) getActivity().getIntent().getParcelableExtra("schedule"));
         viewPager.setAdapter(adapter);
+        Calendar calendar = Calendar.getInstance();
+        int day = calendar.get(Calendar.DAY_OF_WEEK);
+        switch (day) {
+            case Calendar.MONDAY: viewPager.setCurrentItem(1); break;
+            case Calendar.TUESDAY: viewPager.setCurrentItem(2); break;
+            case Calendar.WEDNESDAY: viewPager.setCurrentItem(3); break;
+            case Calendar.THURSDAY: viewPager.setCurrentItem(4); break;
+            case Calendar.FRIDAY: viewPager.setCurrentItem(5); break;
+        }
         info = (TextView) view.findViewById(R.id.tvScheduleInfo);
         Schedule schedule = getActivity().getIntent().getParcelableExtra("schedule");
         info.setText("Группа: "+schedule.getName()+" Cмена: "+schedule.getShift());
